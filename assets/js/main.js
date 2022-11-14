32090be0fb1aacc445a8adee64ffe542bc37ea5a
Ahmed ayob <108896341+thecoder512@users.noreply.github.com>
2022-11-14 17:19:45 +0200

    Initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,152 @@
+
+/*=============== CHANGE BACKGROUND HEADER ===============*/ 
+function scrollHeader(){
+    const header = document.getElementById('header')
+    //when the scroll is greater than 50 viewport height, add the scroll-header class to theheader tag
+    if(this.scrollY >= 50) header.classList.add('header__swipe'); else header.classList.remove('header__swipe')
+}
+window.addEventListener('scroll', scrollHeader)
+
+/*=============== SWIPER POPULAR ===============*/ 
+var swiper = new Swiper(".popular__container", {
+  slidesPerView: 3,
+  spaceBetween: 25,
+  loop: true,
+  centerSlide: 'true',
+  fade: 'true',
+  grabCursor: 'true',
+
+  navigation: {
+    nextEl: ".button-next",
+    prevEl: ".button-prev",
+  },
+
+  breakpoints:{
+      0: {
+          slidesPerView: 1,
+      },
+      520: {
+          slidesPerView: 3,
+      },
+      950: {
+          slidesPerView: 3,
+      },
+  },
+});
+
+/*=============== value accordion ===============*/
+const accordionItems = document.querySelectorAll('.value__acccordion-item');
+
+accordionItems.forEach((item) =>{
+  const accordionHeader = item.querySelector('.value__accordion-header')
+  accordionHeader.addEventListener('click', () =>{
+    const openItem = document.querySelector('.accordion-open')
+
+    toggleItem(item)
+
+    if(openItem && openItem!== item){
+      toggleItem(openItem)
+    }
+  })
+})
+
+const toggleItem = (item) =>{
+  const accordionContent = item.querySelector('.value__accordion-content');
+
+  if(item.classList.contains('accordion-open')){
+    accordionContent.removeAttribute('style')
+    item.classList.remove('accordion-open')
+  }else{
+    accordionContent.style.height = accordionContent.scrollHeight + 'px'
+    item.classList.add('accordion-open')
+  }
+}
+
+/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections = document.querySelectorAll('section[id]')
+    
+const scrollActive = () =>{
+  	const scrollY = window.pageYOffset
+
+	sections.forEach(current =>{
+		const sectionHeight = current.offsetHeight,
+			  sectionTop = current.offsetTop - 58,
+			  sectionId = current.getAttribute('id'),
+			  sectionsClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']')
+
+		if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+			sectionsClass.classList.add('active-link')
+		}else{
+			sectionsClass.classList.remove('active-link')
+		}                                                    
+	})
+}
+window.addEventListener('scroll', scrollActive)
+
+/*=============== SHOW SCROLL UP ===============*/ 
+const scrollUp = () =>{
+	const scrollUp = document.getElementById('scroll-up')
+    // When the scroll is higher than 350 viewport height, add the show-scroll class to the a tag with the scrollup class
+	this.scrollY >= 350 ? scrollUp.classList.add('show-scroll')
+						: scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
+
+/*=============== DARK LIGHT THEME ===============*/ 
+const themeButton = document.getElementById('theme-button')
+const darkTheme = 'dark-theme'
+const iconTheme = 'bx-sun'
+
+// Previously selected topic (if user selected)
+const selectedTheme = localStorage.getItem('selected-theme')
+const selectedIcon = localStorage.getItem('selected-icon')
+
+// We obtain the current theme that the interface has by validating the dark-theme class
+const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light'
+const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'bx bx-moon' : 'bx bx-sun'
+
+// We validate if the user previously chose a topic
+if (selectedTheme) {
+  // If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the dark
+  document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
+  themeButton.classList[selectedIcon === 'bx bx-moon' ? 'add' : 'remove'](iconTheme)
+}
+
+// Activate / deactivate the theme manually with the button
+themeButton.addEventListener('click', () => {
+    // Add or remove the dark / icon theme
+    document.body.classList.toggle(darkTheme)
+    themeButton.classList.toggle(iconTheme)
+    // We save the theme and the current icon that the user chose
+    localStorage.setItem('selected-theme', getCurrentTheme())
+    localStorage.setItem('selected-icon', getCurrentIcon())
+})
+
+/*=============== SCROLL REVEAL ANMATION ===============*/ 
+const sr = ScrollReveal({
+  origin: 'top',
+  distance: '60px',
+  duration: 2500,
+  delay: 400,
+  // reset: true
+})
+
+sr.reveal('.home__title, .popular__container, .section__subtitle, .subscribe__container, .footer__container')
+sr.reveal('.home__description, .section__title, .footer__info', { delay: 500 })
+sr.reveal('.search__box', { delay: 600 })
+sr.reveal('.value__box', { delay: 700 })
+sr.reveal('.home__imgages', { delay: 800, origin:'bottom'})
+sr.reveal('.logos__img', {interval: 100})
+sr.reveal('.value__images, .contact__content', {origin: 'left'})
+sr.reveal('.value__content, .contact__images', {origin: 'right'})
+
+
+
+
+
+
+// ScrollReveal({ reset: true });
+
+// ScrollReveal().reveal('.home__title');
+// ScrollReveal().reveal('.home__description', { delay: 500 });
+// ScrollReveal().reveal('.value__box', { delay: 2000 });
